9723c5b09d8af293f165eeb6929ea54b027ec59b
Simon FONGARNAND <simon.fongarnand@student.junia.com>
2025-10-03 23:09:26 +0200

    Add taskbarWidgets.png image asset

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ startMenu.addEventListener("mouseout", () => {
 startMenu.addEventListener("click", () => {
     startMenuButton.className = "startMenuButton-onClick";
 })
+
+
+function updateTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const PM = (hours >= 12);
+    if (PM) {
+        document.getElementById('taskbarClock').textContent = `${hours-12}:${minutes} PM`;
+    } else {
+        document.getElementById('taskbarClock').textContent = `${hours}:${minutes} AM`;
+    }
+}
+
+updateTime()
+setInterval(updateTime, 1000);
